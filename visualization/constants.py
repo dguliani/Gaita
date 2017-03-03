e7e00ce9ea8ed9be6deffc0c7a831152a4245e75
dguliani <dhruv.guliani@gmail.com>
2017-03-02 21:37:40 -0500

    setting up infrastructure for visualizations

---
+++ visualization/constants.py
@@ -0,0 +1,19 @@
+# Indices
+TIME_IND    = 0
+AX_IND      = 1
+AY_IND      = 2
+AZ_IND      = 3
+GX_IND      = 4
+GY_IND      = 5
+GZ_IND      = 6
+MX_IND      = 7
+MY_IND      = 8
+MZ_IND      = 9
+ROLL_IND    = 10
+PITCH_IND   = 11
+YAW_IND     = 12
+FSR1_IND    = 13
+FSR2_IND    = 14
+FSR3_IND    = 15
+
+LOG_PATH = ""
