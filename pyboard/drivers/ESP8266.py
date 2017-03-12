932c19f1b2ad2302914924c952b2e418f23eba68
dguliani <dhruv.guliani@gmail.com>
2017-03-12 16:25:37 -0400

    Bringing in working wifi and improving driver

--- pyboard/drivers/ESP8266.py
+++ pyboard/drivers/ESP8266.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
 
-# import logging
-# import sys, serial
-# from time import *
-# import datetime, string
 import sys
 from pyb import UART
 import pyb
 import struct
+import constants
 
-
-UART_DEFAULT_PORT = 4
-ESP8266_DEFAULT_BAUDRATE = 9600
-DEVEL_SSID = "TellMyWifiLover"
-DEVEL_PWD = "pineapple"
-UDP_DEFAULT_IP = "127.0.0.1"
+UART_DEFAULT_PORT = 2
+ESP8266_DEFAULT_BAUDRATE =  115200 #Experimentally derived for our ESP8266 chips
+DEVEL_SSID = "TellMyWifiLover" #"NETGEAR75"
+DEVEL_PWD = "pineapple" #"rockylotus847"#
+#UDP_DEFAULT_IP = "127.0.0.39"
+UDP_DEFAULT_IP = "192.168.0.100" #this should be the broadcast address of the laptop to stream to
 UDP_DEFAULT_PORT = 5005
 
 def enum(**enums):
     return type('Enum', (), enums)
 
-Status = enum(ERR='ERROR', OK=['OK', 'ready', 'no change'], BUSY='busy')
+Status = enum(ERR='ERROR', OK=['OK', 'ready', 'no change', 'SEND OK'], BUSY='busy')
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class ESP8266(object):
@@ -30,50 +27,113 @@ class ESP8266(object):
 
         print("Starting wifi module")
         self.serial = UART(port, baud_rate)
+        self.ssid = ssid
+        self.pwd = pwd
+        self.IP = None
+        self.connected = False
 
-        # self.IP = None
-        self._serial_send( "AT" )
-        # send_cmd( "AT+RST", 5 ) # NOTE: seems to cause problems that require manually reset (pulling down the RST pin)
-        # sleep(3)
-        self._serial_send( "AT+CWMODE=1" ) # set device mode (1=client, 2=AP, 3=both)
-        #The mode will be changed on Olimex MOD-WIFI-ESP8266-DEV only after a reset
-        #The command below will reset the device
-        self._serial_send( "AT+RST");
-        self._serial_send( "AT+CWLAP", 10) # scan for WiFi hotspots
-        # print("Trying to connect to {} with pwd {}".format(ssid, pwd))
-        self._serial_send( "AT+CWJAP=\""+ssid+"\",\""+pwd+"\"", 5 ) # connect
-        self.IP = self._serial_send( "AT+CIFSR", 5) # check IP address
+        self.startup_procedure()
+        # self.udb_send_sample_num_stream()
 
-    def _serial_send(self, command, ack=True, waitTm=1, retry=1): #todo
-        print("Sending command {}".format(command))
-        for i in range(retry):
-            self.serial.write(command +"\r\n" )
-            pyb.delay(20)
+    # TODO ADD Retry
+    # Set expected_return parameter to None in order to retrieve full response
+    def _serial_send2(self, command, expected_return=Status.OK, timeout=1000):
+        self.serial.write(command + "\r\n")
+        response_received = 0
+        # TODO add timeout to this infinite waiting for response
+        start_time = pyb.millis();
+        time = 0;
+        while(response_received == 0 and time < timeout):
+            # Small delay between response checks
+            time = pyb.elapsed_millis(start_time)
+
+            pyb.delay(80)
             ret = self.serial.readline()
-            # print("The ret is: {}".format(struct.unpack('i', ret)))
-            # print("The ret is: {}".format(struct.unpack('b', ret)))
-            # print("The ret is: {}".format(struct.unpack('c', ret)))
-            # print("The ret is: {}".format(struct.unpack('s', ret)))
-            # print("The ret is: {}".format(struct.unpack('p', ret)))
-            print("Got return value {}".format(str(ret)))
-            while('busy' in ret):
+            if(ret == None):
                 ret = self.serial.readline()
-                if( ret in Status.OK): break
-                if( ret in Status.ERR ): break
+            else:
+                ret = ret.decode("utf-8")
+                # print(len(ret))
+                if( expected_return is None):
+                    if( '\n' in ret and len(ret) < 4):
+                        pass # Got carriage return, wait for other response
+                    elif(command in ret):
+                        pass # Sometimes you get the command returned; this is not what we are looking for
+                    else:
+                        return(True, ret)
+                else:
+                    if( ret.strip() in expected_return):
+                        return (True, ret)
+                    elif(Status.ERR in ret): # Maybe should through error here
+                        return (False, ret)
+                    else:
+                        pass
+        return(False,'Timeout')
 
-        return ret
-            # pyb.delay()
+    def startup_procedure(self):
+        passed, ret = self._serial_send2( "AT", expected_return=Status.OK )
+        if passed:
+            passed, ret = self._serial_send2( "AT+CWMODE=1") # set device mode (1=client, 2=AP, 3=both)
+            passed, ret = self._serial_send2( "AT+CWJAP=\""+self.ssid+"\",\""+self.pwd+"\"" ) # connect
+            passed, ret = self._serial_send2( "AT+CIFSR", expected_return=None) # check IP address
+            if passed:
+                self.IP = ret
+                print("Connected to {} with IP: {}".format(self.ssid, ret))
+                self.connected = True
+            else:
+                print("Failed to connect: {}".format(ret))
+                self.connected = False
+
+    def chunk_and_send(self):
+        pass
+
+    def _chunk(self):
+        pass
+    # UDP Functions
+    # TODO Something wrong with this reset
+    def reset_wifi(self):
+        self._serial_send2('AT+RST')
+        pyb.delay(constants.ESP_ST_DELAY)
 
     def udp_connect(self):
         # Setup UDP Connection
-        self._serial_send("AT+CIPMUX=1") # single connection mode
-        self._serial_send("AT+CIPSTART=1,\"UDP\",\"" + UDP_DEFAULT_IP + "\",5005")
+        if self.connected:
+            passed, ret = self._serial_send2("AT+CIPMUX=0") # single connection mode
+            passed, ret = self._serial_send2("AT+CIPSTART=\"UDP\",\"" + UDP_DEFAULT_IP + "\",5005", timeout=10000)
+            if not passed:
+                print("Could not establish UDP connection: {}".format(ret))
+            else:
+                return True
+        else:
+            return False
 
     def udp_send(self, data):
-        cmd = "AT+CIPSEND="+str(1)
-        self._serial_send(cmd)
-        # data = ">" + data
-        self._serial_send(data)
+        data_length = len(data)
+        cmd = "AT+CIPSEND="+str(data_length)
+        print("Trying to send data of length {}".format(str(data_length)))
+        passed, ret = self._serial_send2(cmd, expected_return=['>'])
+        if passed:
+            data = data
+            passed, ret = self._serial_send2(data)
+            if passed:
+                print("Sent: {}".format(data))
+            else:
+                print("Send Error: {}".format(ret))
+        else:
+            print("Unable to send, not recieving > from ESP: {}".format(ret))
+
 
     def udp_close(self):
-        self._serial_send("AT+CIPCLOSE=1")
+        self._serial_send2("AT+CIPCLOSE")
+
+    def udb_send_sample_num_stream(self):
+        if self.connected and self.udp_connect():
+            data_counter = 990
+            while(data_counter < 1000):
+                self.udp_send(str(data_counter) + "LALALALALALALALALALALAL\r\n")
+                data_counter = data_counter + 1
+            self.udp_close()
+
+if __name__ == "__main__":
+    ESP8266_obj = ESP8266()
+    del ESP8266_obj
--- udp_read.py
+++ udp_read.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python
+# TO DO
+# Make sure you're on the TellMyWiFiLover network
 import socket
 
-UDP_IP = "127.0.0.1"
+#this should be the IP address of the laptop being used
+#MAKE SURE YOU'RE ON THE SAME NETWORK
+UDP_IP = "192.168.0.100" #"192.168.1.255"
+#UDP_IP = "192.168.1.255"
+#UDP_IP="127.0.0.1"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -8,5 +15,19 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    #data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(16) # buffer size is 32
     print "received message:", data
+
+# import socket
+
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005
+
+# sock = socket.socket(socket.AF_INET, # Internet
+#                      socket.SOCK_DGRAM) # UDP
+# sock.bind((UDP_IP, UDP_PORT))
+
+# while True:
+#     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#     print "received message:", data
