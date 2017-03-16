15cc4b1e42f5354deb25d0149cc777cf94922c18
dguliani <dhruv.guliani@gmail.com>
2017-03-16 15:04:57 -0400

    updated plots and data

--- postprocessing/gait_extraction.m
+++ postprocessing/gait_extraction.m
@@ -1,143 +1,224 @@
-clear all 
-close all 
+function [] = gait_extraction(file)
+    close all 
 
-% This file will only works on data collected after Feb 6. 
-
-addpath('DriftStudyData');
-addpath('RawLogs');
-
-% Read in the new data
-% cal_data = csvread('16_01_17_raw_log_stationary.csv');
-data = csvread('05_03_17_sherry_walk_flat.csv');
-
-time = data(:,1)/1000; 
-ax = data(:,2); ay = data(:,3); az = data(:,4);
-gx = data(:,5); gy = data(:,6); gz = data(:,7);
-mx = data(:,8); my = data(:,9); mz = data(:,10);
-roll = data(:,11); pitch = data(:,12); yaw = data(:,13); 
-fsr1 = data(:,14); fsr2 = data(:,15); fsr3 = data(:,16); 
-
-mean_dt = mean(diff(time,[],1));  
-mean_dt = 0.01;
-fs = 1/mean_dt;
-
-% Axis correction (fix later) 
-% yaw = yaw.*(pi/180); % About z
-% roll = roll.*(pi/180); % About y
-% pitch = pitch.*(pi/180); % About x
-% 
-% for i=1:length(ax)
-%     rz = rotz(yaw(i));
-%     ry= rotz(roll(i));
-%     rx = rotz(pitch(i));
-%     
-%     rot = rz*ry*rx;
-%     
-%     a_temp = [ax(i); ay(i); az(i)];
-%     a_new = rot*a_temp;
-%     ax(i) = a_new(1);
-%     ay(i) = a_new(2);
-%     az(i) = a_new(3);
-% end
-
-
-% Highpass filter design 
-fc = 0.2;
-[b,a] = butter(6,fc/(fs/2),'high');
-% filter = tf(b, a);
-% freqz(b,a)
-ax = filter(b,a,ax); ay = filter(b,a,ay); az = filter(b,a,az);
-filt = tf(b, a);
-
-ind = split_step_indices([fsr1 fsr2 fsr3], 0.5);
-
-% Array of Step Times 
-step_time = [];
-step_length = []; 
-step_height = []; 
-
-% Kalman Filter for Positioning    
-A = [1      mean_dt     -(mean_dt^2)/2;...
-     0      1           -mean_dt;...
-     0      0           1];
-
-B = [mean_dt^2/2; mean_dt; 0];
-
-C = [1 mean_dt -(mean_dt^2)/2];
-
-Plant = ss(A,[B B],C,0,-1,'inputname',{'u' 'w'},'outputname','y');
-Q = 1;
-R = 1;
-[kalmf,L,P,M] = kalman(Plant,Q,R);
-
-a = A;
-b = [B B 0*B];
-c = [C;C];
-d = [0 0 0;0 0 1];
-Pl = ss(a,b,c,d,-1,'inputname',{'u' 'w' 'v'},'outputname',{'y' 'yv'});  
-sys = parallel(Pl,kalmf,1,1,[],[]);
-SimModel = feedback(sys,1,4,2,1);   % Close loop around input #4 and output #2
-SimModel = SimModel([1 3],[1 2 3]); % Delete yv from I/O list
-
-
-figure(2)
-for i=1:(length(ind)/2)
-    s = ind(i*2 - 1);
-    e = ind(i*2);
+    % This file will only works on data collected after Feb 6. 
+    addpath('Raw');
     
-    % Computing Step Time
-    step_time(i) = time(e) - time(s); 
-    
-    num_pts = length(time(s:e));
-    
-    rng default
-    w = zeros(num_pts, 1); %sqrt(Q)*randn(n,1);
-    v = zeros(num_pts, 1); %sqrt(R)*randn(n,1);
+    save_file = 1; 
+    % Read in the new data
+    % cal_data = csvread('16_01_17_raw_log_stationary.csv');
+    data = csvread(file);
 
-    [kpx,x] = lsim(SimModel,[w,v,ax(s:e)]);
-    [kpy,y] = lsim(SimModel,[w,v,ay(s:e)]);
-    [kpz,z] = lsim(SimModel,[w,v,az(s:e)]);
+    time = data(:,1)/1000; 
+    ax = data(:,2); ay = data(:,3); az = data(:,4);
+    gx = data(:,5); gy = data(:,6); gz = data(:,7);
+    mx = data(:,8); my = data(:,9); mz = data(:,10);
+    roll = data(:,11); pitch = data(:,12); yaw = data(:,13); 
+    fsr1 = data(:,14); fsr2 = data(:,15); fsr3 = data(:,16); 
+
+    mean_dt = mean(diff(time,[],1));  
+    mean_dt = 0.013; % Hacky - fix with own KF
+    fs = 1/mean_dt;
+
+    % Highpass filter design 
+    fc = 0.2;
+    [b,a] = butter(6,fc/(fs/2),'high');
+    % filter = tf(b, a);
+    % freqz(b,a)
+    ax = filter(b,a,ax); ay = filter(b,a,ay); az = filter(b,a,az);
+    filt = tf(b, a);
+
+    ind = split_step_indices([fsr1 fsr2 fsr3], 0.5);
+
+    % Array of Gait Factors 
+    step_time = [];
+    cadence = [];
+    step_length = []; 
+    step_height = [];
+    foot_angle = []; % Records max yaw
+    accel_planar = []; 
+    accel_vertical = []; 
+    
+    step_l_time = zeros(length(ax), 1); 
+    step_h_time = zeros(length(ax), 1); 
+
+    % Kalman Filter for Positioning    
+    A = [1      mean_dt     -(mean_dt^2)/2;...
+         0      1           -mean_dt;...
+         0      0           1];
+
+    B = [mean_dt^2/2; mean_dt; 0];
+
+    C = [1 mean_dt -(mean_dt^2)/2];
+
+    Plant = ss(A,[B B],C,0,-1,'inputname',{'u' 'w'},'outputname','y');
+    Q = 0.03;
+    R = 0.02;
+    [kalmf,L,P,M] = kalman(Plant,Q,R);
+
+    a = A;
+    b = [B B 0*B];
+    c = [C;C];
+    d = [0 0 0;0 0 1];
+    Pl = ss(a,b,c,d,-1,'inputname',{'u' 'w' 'v'},'outputname',{'y' 'yv'});  
+    sys = parallel(Pl,kalmf,1,1,[],[]);
+    SimModel = feedback(sys,1,4,2,1);   % Close loop around input #4 and output #2
+    SimModel = SimModel([1 3],[1 2 3]); % Delete yv from I/O list
+    
+    count = 1; 
+    figure(2)
+    for i=1:(length(ind)/2)
+        s = ind(i*2 - 1);
+        e = ind(i*2);
+
+        yaw_start = yaw(s); 
+        yaw_local = yaw(s:e) - yaw(s); 
+        % Axis correction (fix later) 
+        yaw_local = yaw_local.*(pi/180); % About z
+        roll_local = roll(s:e).*(pi/180); % About y
+        pitch_local = pitch(s:e).*(pi/180); % About x
+
+        ax_local = ax(s:e);
+        ay_local = ay(s:e);
+        az_local = az(s:e);
+
+        for j=1:length(s:e)
+            rz = rotz(yaw_local(j));
+            ry= roty(roll_local(j));
+            rx = rotx(pitch_local(j));
+
+            rot = rz*ry*rx;
+
+            a_temp = [ax_local(j); ay_local(j); az_local(j)];
+            a_new = rot*a_temp;
+            ax_local(j) = a_new(1);
+            ay_local(j) = a_new(2);
+            az_local(j) = a_new(3);
+        end
+
+        num_pts = length(time(s:e));
+
+        w = zeros(num_pts, 1); %sqrt(Q)*randn(n,1);
+        v = zeros(num_pts, 1); %sqrt(R)*randn(n,1);
+
+        [kpx,x] = lsim(SimModel,[w,v,ax_local]);
+        [kpy,y] = lsim(SimModel,[w,v,ay_local]);
+        [kpz,z] = lsim(SimModel,[w,v,az_local]);
+
+        ye_x = kpx(:,2); % + v;     % measured response
+        ye_y = kpy(:,2); % + v;     % measured response
+        ye_z = kpz(:,2); % + v;     % measured response
+
+        sl = sqrt(ye_x.^2 + ye_y.^2);
+
+        % Saving Gait Factors for Static Plots 
+        if((time(e) - time(s)) > 0.1)
+            step_height(count) = max(ye_z);
+            step_length(count) = max(sl);
+            step_time(count) = time(e) - time(s); 
+            cadence(count) = 60/step_time(count);
+            foot_angle(count) = max(yaw_local);
+            accel_planar(count) = max(max(ax_local), max(ay_local));
+            accel_vertical(count) = max(az_local); 
+            count = count+1;
+        end
+        
+        step_l_time(s:e) = sl; 
+        step_h_time(s:e) = ye_z;
+        
+        % Saving Gait Factors for Live Plots 
+
+        subplot(3,1,1);
+        plot(time(s:e) - time(s), ye_x); hold on;
+        subplot(3,1,2);
+        plot(time(s:e) - time(s), ye_y); hold on;
+        subplot(3,1,3);
+        plot(time(s:e) - time(s), ye_z); hold on;
+    end 
+    
+    % outlier removal
+
+    mean_dt = mean(diff(time,[],1)); 
+    fs = 1/mean_dt; % 100 Hz Sampling
+    num_pts = length(time); 
+    acc_raw = [ax ay az]; 
+
+    figure(1)
+    subplot(4,1,1); hold on; grid on; 
+    scatter([1:length(step_time)], step_time, 'r');
+    xlabel('Step Number'); 
+    ylabel('Step Time [s]');  
+    title('Step Times'); 
+
+    subplot(4,1,2); hold on; grid on; 
+    scatter(step_length, step_height, 'b'); 
+    xlabel('Step Length'); 
+    ylabel('Step Height');  
+    % title('Step Times'); 
+    % plot(time, fsr2, 'g');
+    % plot(time, fsr3, 'b');
+    % plot(time, temp);
+    % legend('x','y','z');
+
+    subplot(4,1,3); hold on; grid on; 
+    scatter(foot_angle.*(180/pi), cadence, 'g'); 
+    xlabel('Foot Angle'); 
+    ylabel('Cadence'); 
+    
+    subplot(4,1,4); hold on; grid on; 
+    scatter(accel_planar, accel_vertical, 'g'); 
+    xlabel('Planar Accel'); 
+    ylabel('Cadence'); 
+    
+%     mean(step_time)
+%     mean(step_length)
+%     mean(step_height)
+    
+    step_num = transpose([1:length(step_time)]);
+     
+    figure(5);
+    ax1 = subplot(3,1,1);
+    plot(time, step_l_time, 'r'); grid on; hold on; 
+    plot(time, step_h_time, 'b'); 
+    legend('Step Length', 'Step Height'); 
+    title('Time domain walk'); 
+    w = zeros(length(time), 1); %sqrt(Q)*randn(n,1);
+    v = zeros(length(time), 1); %sqrt(R)*randn(n,1);
+
+    [kpx,x] = lsim(SimModel,[w,v,ax]);
+    [kpy,y] = lsim(SimModel,[w,v,ay]);
+    [kpz,z] = lsim(SimModel,[w,v,az]);
 
     ye_x = kpx(:,2); % + v;     % measured response
     ye_y = kpy(:,2); % + v;     % measured response
     ye_z = kpz(:,2); % + v;     % measured response
     
-    sl = sqrt(ye_x.^2 + ye_y.^2)
-    step_height(i) = max(ye_z);
-    step_length(i) = max(sl);
+    ax2 = subplot(3,1,2);
+    plot(time, sqrt(ye_x.^2 + ye_y.^2), 'r'); grid on; hold on; 
+    plot(time, ye_z, 'b'); 
+    legend('Step Length', 'Step Height'); 
     
-    subplot(3,1,1);
-    plot(time(s:e) - time(s), ye_x); hold on;
-    subplot(3,1,2);
-    plot(time(s:e) - time(s), ye_y); hold on;
-    subplot(3,1,3);
-    plot(time(s:e) - time(s), ye_z); hold on;
-end 
+    ax3 = subplot(3,1,3); 
+    plot(time, pitch); 
+    grid on; hold on; 
+    legend('Pitch (deg)');
+    
+    linkaxes([ax1,ax2, ax3], 'x'); 
+    raw_plot(data);
+    if( save_file )
+        
+        fsr1 = smooth(fsr1, 15);
+        fsr2 = smooth(fsr2, 15);
+        fsr3 = smooth(fsr3, 15);
+        static_factors = [step_num, transpose(step_time) transpose(step_length),...
+                        transpose(step_height), transpose(foot_angle),...
+                        transpose(accel_planar), transpose(accel_vertical)]; 
 
-mean_dt = mean(diff(time,[],1)); 
-fs = 1/mean_dt; % 100 Hz Sampling
-num_pts = length(time); 
-acc_raw = [ax ay az]; 
+        dyn_factors = [time, step_l_time, step_h_time, pitch, fsr1, fsr2, fsr3];
 
-figure(1)
-subplot(2,1,1); hold on; grid on; 
-scatter([1:length(step_time)], step_time, 'r');
-xlabel('Step Number'); 
-ylabel('Time [s]');  
-title('Step Times'); 
-
-subplot(2,1,2); hold on; grid on; 
-scatter(step_length, step_height, 'b'); 
-xlabel('Step Length'); 
-ylabel('Step Height');  
-% title('Step Times'); 
-% plot(time, fsr2, 'g');
-% plot(time, fsr3, 'b');
-% plot(time, temp);
-% legend('x','y','z');
-
-mean(step_time)
-mean(step_length)
-mean(step_height)
-
-raw_plot(data);
+        static_path = strcat('Processed/static_',file);
+        dyn_path = strcat('Processed/dynamic_',file);
+        csvwrite(static_path,static_factors);
+        csvwrite(dyn_path,dyn_factors);
+    end
+end
--- visualization/plot_test.py
+++ visualization/plot_test.py
@@ -11,26 +11,103 @@ import plotly.plotly as py
 import plotly.tools as tls
 import plotly.graph_objs as go
 
+import threading
+import csv
+
 class Dashboard(object):
     def __init__(self):
-        # self.create_step_stream()
-        self.clear_stream()
-        self.stream_random()
-        # self.create_3d_cluster()
+        self.create_step_stream()
+        self.create_angle_stream()
+        self.create_pressure_map()
+        time.sleep(const.PY_STANDARD_DELAY)
 
-    def clear_stream(self):
+        self.clear_all_streams()
+        self.threads = []
+
+        f_path = "/Users/dguliani/Documents/Development/workspace/Gaita/postprocessing/Processed/dynamic_05_03_17_shahid_walk_flat.csv"
+        stream_thread = threading.Thread(target=self.stream_from_file, args=(f_path,))
+        self.threads.append(stream_thread)
+        stream_thread.start()
+
+        stream_thread.join()
+        self.threads.remove(stream_thread)
+        # self.stream_from_file(f_path)
+
+
+    def clear_all_streams(self):
+        print "Clearing All Streams"
         s1 = py.Stream(const.PY_STEPH_STREAM)
         s2 = py.Stream(const.PY_STEPL_STREAM)
-
+        s3 = py.Stream(const.PY_STEPA_STREAM)
+        s4 = py.Stream(const.PY_PRESS_STREAM)
         # We then open a connection
         s1.open()
         s2.open()
+        s3.open()
+        s4.open()
 
         s1.write(dict(x=[],y=[]))
         s2.write(dict(x=[],y=[]))
+        s3.write(dict(x=[],y=[]))
+        s4.write(dict(x=[],y=[]))
 
         s1.close()
         s2.close()
+        s3.close()
+        s4.close()
+
+        time.sleep(const.PY_STANDARD_DELAY)
+
+    def stream_from_file(self, f_path):
+        s1 = py.Stream(const.PY_STEPH_STREAM)
+        s2 = py.Stream(const.PY_PRESS_STREAM)
+        s3 = py.Stream(const.PY_STEPA_STREAM)
+
+        s1.open()
+        s2.open()
+        s3.open()
+
+        k = 5    # some shape parameter
+
+        with open(f_path, 'rb') as csvfile:
+            print ("Opened file")
+            spamreader = csv.reader(csvfile)
+            # row_count = sum(1 for row in spamreader)
+            count = 0
+            print "reading rows"
+            for row in spamreader:
+                if count is not 0:
+                    t = float(row[0])
+                    step_length = float(row[1])
+                    step_height = float(row[2])
+                    pitch = float(row[3])
+                    fsr_frontl = float(row[4])*const.PY_PRESS_MULTIPLIER
+                    fsr_back = float(row[5])*const.PY_PRESS_MULTIPLIER
+                    fsr_frontr = float(row[6])*const.PY_PRESS_MULTIPLIER
+
+
+                    s1.write(dict(x=t, y=step_height))
+                    s3.write(dict(x=t, y=pitch))
+                    trace = dict(x=[-1,0,1], y=[8,3,8],
+                                 marker=dict(
+                                     size = [fsr_frontl,fsr_back,fsr_frontr],
+                                 )
+                    )
+                    s2.write(trace)
+
+                    time.sleep(t - last_t)
+
+                    if t > 15:
+                        "Stopping stream after 60 seconds"
+                        break
+
+                last_t = float(row[0])
+                count = count + 1
+
+        s1.close()
+        s2.close()
+        s3.close()
+        print f_path
 
     def stream_random(self):
         print("Sending Random Data")
@@ -75,10 +152,49 @@ class Dashboard(object):
         s1.close()
         s2.close()
 
+    def create_pressure_map(self):
+        print("Creating Pressure Map")
+        stream_id = dict(token=const.PY_PRESS_STREAM, maxpoints=5)
+
+        trace = go.Scatter(
+            x=[-1,0,1],
+            y=[8,3,8],
+            mode='markers',
+            name = 'Step Length',
+            marker=dict(
+                size = [10,40,10],
+                color = 'red',
+            ),
+            stream=stream_id
+        )
+
+        data = go.Data([trace])
+
+        # Add title to layout object
+        layout = go.Layout(title='Live Pressure Map',
+                           xaxis=dict(title='x',
+                                      range=[-4, 4],
+                                      showline=False,
+                                      showgrid=False,
+                                      zeroline=False,
+                                      showticklabels=False),
+                           yaxis=dict(title='y',
+                                      range=[0,12],
+                                      showline=False,
+                                      showgrid=False,
+                                      zeroline=False,
+                                      showticklabels=False))
+
+        # Make a figure object
+        fig = go.Figure(data=data, layout=layout)
+
+        # Send fig to Plotly, initialize streaming plot, open new tab
+        py.plot(fig, filename='Live Pressure Map')
+
     def create_step_stream(self):
-        print("Resetting Both Streams")
-        stream_id1 = dict(token=const.PY_STEPH_STREAM, maxpoints=30)
-        stream_id2 = dict(token=const.PY_STEPL_STREAM, maxpoints=30)
+        print("Creating Step Height and Length Streams")
+        stream_id1 = dict(token=const.PY_STEPH_STREAM, maxpoints=500)
+        stream_id2 = dict(token=const.PY_STEPL_STREAM, maxpoints=500)
 
         trace_1 = go.Scatter(
             x=[],
@@ -99,15 +215,41 @@ class Dashboard(object):
         data = go.Data([trace_1, trace_2])
 
         # Add title to layout object
-        layout = go.Layout(title='Live Steps',
+        layout = go.Layout(title='Live Step Height',
                            xaxis=dict(title='Time'),
-                           yaxis=dict(title='Distance (m)'))
+                           yaxis=dict(title='Step Height (m)'))
 
         # Make a figure object
         fig = go.Figure(data=data, layout=layout)
 
         # Send fig to Plotly, initialize streaming plot, open new tab
-        py.plot(fig, filename='Live Steps')
+        py.plot(fig, filename='Live Step Parameters')
+
+    def create_angle_stream(self):
+        print("Creating Step Angle Streams")
+        stream_id = dict(token=const.PY_STEPA_STREAM, maxpoints=500)
+
+        trace = go.Scatter(
+            x=[],
+            y=[],
+            mode='',
+            name ='Foot Angle',
+            stream=stream_id         # (!) embed stream id, 1 per trace
+        )
+
+
+        data = go.Data([trace])
+
+        # Add title to layout object
+        layout = go.Layout(title='Live Foot Angle',
+                           xaxis=dict(title='Time [s]'),
+                           yaxis=dict(title='Foot Angle (Pitch) [deg]'))
+
+        # Make a figure object
+        fig = go.Figure(data=data, layout=layout)
+
+        # Send fig to Plotly, initialize streaming plot, open new tab
+        py.plot(fig, filename='Live Step Angles')
 
     def create_3d_cluster(self):
         df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/alpha_shape.csv')
